3aae2c69e2d0b31f8391fb0bc8970fb2fcee0e8b
sylviavu <47538028+sylviavu@users.noreply.github.com>
2021-04-06 18:24:26 -0500

    Added to Registration view function

--- api/routes.py
+++ api/routes.py
@@ -3,10 +3,15 @@ FILE: app/routes.py
 
 This file will contain decorators and their functions
 """
-from flask import flash, redirect, render_template, url_for, jsonify
-from api import app
-from api.forms import LoginForm
+
+from flask import flash, redirect, render_template, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import Hardware, Project, User, create_user
+from flask import jsonify
 from flask_login import current_user, login_user, logout_user, login_required
+
+
 
 
 @app.route('/')
@@ -17,24 +22,32 @@ def home():
     # Mock homepage below
     user = {'username': 'Sylvia'}
 
-    return jsonify(user)       # Jinja2 substitutes {{ ... }} in the templates with the parameters here
+    return jsonify(user)
     
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     # TODO: Render registration page
     # TODO: Implement OAuth
+    """ Fair warning: Internal Server Error will occur if you try to submit a registration form """
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))        # redirect logged-in users for now --> this can change to a user portal later
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        create_user(form.username.data, form.email.data, form.password.data)
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
     
-    
-    return "This is the registration page!"
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # TODO: Render login page
     # TODO: Implement OAuth 
-    
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     # Mock login page below
     form = LoginForm()      # instantiate object from LoginForm class
 
@@ -44,5 +57,10 @@ def login():
         return redirect(url_for('home'))
 
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    pass
     
 

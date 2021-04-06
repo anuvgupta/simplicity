4c9b96e5e06bb3bb995aeb28b8af964bf5070348
sylviavu <47538028+sylviavu@users.noreply.github.com>
2021-04-06 18:41:39 -0500

    Changed MongoEngine initialization

--- api/__init__.py
+++ api/__init__.py
@@ -9,19 +9,28 @@ import os
 
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 from flask_cors import CORS #comment this on deployment
 from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"        # connect to the MongoDB server to myDatabase --> database exposed as "db" attribute
-mongo = PyMongo(app)
+""" Using MongoEngine instead of PyMongo but keeping this initialization in case stuff goes wrong """
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"        # connect to the MongoDB server to myDatabase --> database exposed as "db" attribute
+# mongo = PyMongo(app)
 # db = mongo.db
+
 app.config['MONGODB_SETTINGS']= {
     "db": "web-app",
 }
-db = MongoEngine(app)
+db = MongoEngine()
+db.init_app(app)    # db initialization occurs before the app starts
+
+login = LoginManager(app)
+login.login_view = 'login'
+
 CORS(app)
 
 

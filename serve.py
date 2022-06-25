5c8f68fb4afc5111f20d251f092fed9398cf1331
anuvgupta <me@anuv.me>
2022-06-25 01:40:17 -0500

    use env port for dash

--- serve.py
+++ serve.py
@@ -1,5 +1,9 @@
+import os
 from waitress import serve
 from api import app
 
+IP = str(os.environ.get('IP', '0.0.0.0'))
+PORT = int(os.environ.get('PORT', 3017))
+
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=3017)
+    serve(app, host=IP, port=PORT)
